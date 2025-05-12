2f5d12ae3f0f0a73bbf0051b8558ac8c6aa21653
KimSeonHui <zero9657@naver.com>
2025-05-12 22:12:38 +0900

    배포 설정

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  output: "export",
+  basePath: "/KimSeonHui.github.io",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
