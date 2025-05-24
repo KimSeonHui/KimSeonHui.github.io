70c784c05450b14a2232a2113635f1cb750b0bc9
KimSeonHui <zero9657@naver.com>
2025-05-24 17:44:41 +0900

    코드 하이라이팅 적용

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,6 +1,7 @@
 import { remark } from "remark";
 import html from "remark-html";
 import gfm from "remark-gfm";
+import hljs from "highlight.js";
 
 export default async function markdownToHtml(
   markdown: string
@@ -17,6 +18,21 @@ export default async function markdownToHtml(
     .process(markdown);
 
   let content = result.toString();
+
+  // 코드 블록에 구문 강조 적용
+  content = content.replace(
+    /<pre><code class="language-(\w+)">([\s\S]*?)<\/code><\/pre>/g,
+    (match, lang, code) => {
+      const validLang = hljs.getLanguage(lang) ? lang : "javascript";
+      const decodedCode = code
+        .replace(/&#x3C;/g, "<")
+        .replace(/&#x3E;/g, ">")
+        .replace(/&#x26;/g, "&");
+      const highlighted = hljs.highlight(validLang, decodedCode).value;
+      return `<pre><code class="language-${validLang} hljs">${highlighted}</code></pre>`;
+    }
+  );
+
   content = content.replace(
     /<li>\s*<input type="checkbox"/g,
     '<li class="task-list-item"><input type="checkbox"'
